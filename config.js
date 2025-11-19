bc72b9270972b27df527b1c94d4305c7c9363445
UdobnaMebel <udobnamebel@gmail.com>
2025-11-19 21:10:47 +0300

    1

---
+++ config.js
@@ -0,0 +1,18 @@
+/* --- НАСТРОЙКИ КОНТЕНТА --- */
+
+// 1. Название файла для обложки сайта (должен лежать в папке images)
+const heroCover = "cover.png    "; 
+
+// 2. Список файлов для слайдера (должны лежать в папке images)
+const sliderImages = [
+    "slide1.png",
+    "slide2.png",
+    "slide3.png",
+    "slide4.png",
+    "slide5.png",
+    "slide1.png",
+    "slide2.png",
+    "slide3.png",
+    "slide4.png",
+    "slide5.png"
+];
